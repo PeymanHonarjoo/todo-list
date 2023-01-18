51f8db333fce9c6d236b8fb5aecfc40716308421
Peyman Honarjoo <peymanhonarjoo1381@gmail.com>
2023-01-19 01:56:44 +0330

    updated: index.js router to redirect to 404View if the page not found

--- src/router/index.js
+++ src/router/index.js
@@ -86,6 +86,18 @@ const routes = [
       requiresAuth: false,
     },
   },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "404",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "signup" */ "../views/404View.vue"),
+    meta: {
+      requiresAuth: false,
+    },
+  },
 ];
 
 const router = new VueRouter({
