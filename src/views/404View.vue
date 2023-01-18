b9b156eb755263d46832d4b8442f2af83bd9dbd7
Peyman Honarjoo <peymanhonarjoo1381@gmail.com>
2023-01-19 02:20:03 +0330

    updated: 404View's text colors

--- src/views/404View.vue
+++ src/views/404View.vue
@@ -1,17 +1,28 @@
 <template lang="">
-  <div
-    class="d-flex justify-center flex-column mt-10"
-    :class="{
-      'grey--text': !$vuetify.theme.dark,
-      'white--text': $vuetify.theme.dark,
-    }"
-  >
-    <h1 class="text-center">404</h1>
-    <div class="text-center">
-      <span class="font-weight-light">Page </span>
-      <span>not </span>
-      <span class="font-weight-light">found</span>
-    </div>
+  <div class="d-flex justify-center flex-column mt-10">
+    <v-card
+      flat
+      outlined
+      class="ma-5 pa-10"
+      :class="{
+        'grey--text text--darken-1': !$vuetify.theme.dark,
+        'white--text': $vuetify.theme.dark,
+      }"
+    >
+      <div class="text-center title">
+        <v-icon class="red--text">mdi-tooltip-remove</v-icon>
+      </div>
+      <h1 class="text-center">
+        <span class="red--text">4</span>
+        <span class="orange--text">0</span>
+        <span class="cyan--text">4</span>
+      </h1>
+      <div class="text-center">
+        <span class="red--text">Page </span>
+        <span class="font-weight-bold orange--text">not </span>
+        <span class="cyan--text">found</span>
+      </div>
+    </v-card>
   </div>
 </template>
 <script>
