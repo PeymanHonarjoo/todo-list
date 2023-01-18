bebbf75f37e2074ad881ba698b81537208868150
Peyman Honarjoo <peymanhonarjoo1381@gmail.com>
2023-01-19 01:58:23 +0330

    updated: 404View to show the '404 page not found' text

---
+++ src/views/404View.vue
@@ -0,0 +1,20 @@
+<template lang="">
+  <div
+    class="d-flex justify-center flex-column mt-10"
+    :class="{
+      'grey--text': !$vuetify.darkmode,
+      'white--text': $vuetify.darkmode,
+    }"
+  >
+    <h1 class="text-center">404</h1>
+    <div class="text-center">
+      <span class="font-weight-light">Page </span>
+      <span>not </span>
+      <span class="font-weight-light">found</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang=""></style>
